986b3b336a93efb29f7ec8bd68da32a8e61b373d
Berkay Kayaoglu <berkay.kayaoglu@commencis.com>
2022-12-15 15:32:04 +0300

    logo update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
-import logo2 from "../assets/img/logo.png";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
+import logo_last from "../assets/img/logo_last.png";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
@@ -12,7 +9,7 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <Col size={18} sm={6}>
-            <img src={logo2} alt="Logo" />
+            <img src={logo_last} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,7 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
-import logo2 from '../assets/img/logo.jpeg';
-import logo3 from '../assets/img/logo.png';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
-import { HashLink } from 'react-router-hash-link';
+
+import logo_last from '../assets/img/logo_last.png';
 import {
   BrowserRouter as Router
 } from "react-router-dom";
@@ -39,7 +34,7 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo3} alt="Logo" />
+            <img src={logo_last} alt="Logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
@@ -48,12 +43,8 @@ export const NavBar = () => {
             <Nav className="ms-auto">
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Anasayfa</Nav.Link>
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Özellikler</Nav.Link>
+              <Nav.Link href="#footer" className={activeLink === 'footer' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>İletişim</Nav.Link>
             </Nav>
-            <span className="navbar-text">
-              <HashLink to='#footer'>
-                <button className="vvd"><span>İletişim</span></button>
-              </HashLink>
-            </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
